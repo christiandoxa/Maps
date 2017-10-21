6cc2be02edbca2f847230640a0da7603d8c32512
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2017-10-22 02:13:20 +0700

    - Grand Canyon App

--- app/src/main/java/com/adroitdevs/maps/MainActivity.java
+++ app/src/main/java/com/adroitdevs/maps/MainActivity.java
@@ -1,60 +1,32 @@
 package com.adroitdevs.maps;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    static final CameraPosition SEATTLE = CameraPosition.builder()
-            .target(new LatLng(47.6204, -122.3491))
-            .zoom(10)
-            .bearing(0)
-            .tilt(45)
-            .build();
-
-    LatLng renton = new LatLng(47.489805, -122.120502);
-    LatLng kirkland = new LatLng(47.7301986, -122.1768858);
-    LatLng everret = new LatLng(47.978748, -122.202001);
-    LatLng lynnwood = new LatLng(47.819533, -122.32288);
-    LatLng montlake = new LatLng(47.7973733, -122.3281771);
-    LatLng kent = new LatLng(47.385938, -122.258212);
-    LatLng showare = new LatLng(47.38702, -122.23986);
-
-    GoogleMap mGoogleMap;
-    boolean mapReady = false;
+public class MainActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    private void flyTo(CameraPosition target) {
-        mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetViewPanoramaFragment);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        mapReady = true;
-        mGoogleMap = map;
-        flyTo(SEATTLE);
-        mGoogleMap.addCircle(new CircleOptions()
-                .center(renton)
-                .radius(5000)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.argb(64, 0, 255, 0)));
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        panorama.setPosition(new LatLng(36.0579667, -112.1430996));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
     }
-
 }
