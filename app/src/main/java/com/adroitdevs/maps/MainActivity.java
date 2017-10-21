a02f184fb6db7e4e6c2721c48ab84c5648b97c6d
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2017-10-21 13:33:49 +0700

    - Adding a Polyline

--- app/src/main/java/com/adroitdevs/maps/MainActivity.java
+++ app/src/main/java/com/adroitdevs/maps/MainActivity.java
@@ -7,10 +7,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -20,7 +19,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             .bearing(0)
             .tilt(45)
             .build();
-    MarkerOptions renton, kirkland, everett, lynnwood, montlake, kent, showare;
+
+    LatLng renton = new LatLng(47.489805, -122.120502);
+    LatLng kirkland = new LatLng(47.7301986, -122.1768858);
+    LatLng everret = new LatLng(47.978748, -122.202001);
+    LatLng lynnwood = new LatLng(47.819533, -122.32288);
+    LatLng montlake = new LatLng(47.7973733, -122.3281771);
+    LatLng kent = new LatLng(47.385938, -122.258212);
+    LatLng showare = new LatLng(47.38702, -122.23986);
+
     GoogleMap mGoogleMap;
     boolean mapReady = false;
 
@@ -28,35 +35,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        renton = new MarkerOptions()
-                .position(new LatLng(47.489805, -122.120502))
-                .title("Renton")
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        kirkland = new MarkerOptions()
-                .position(new LatLng(47.7301986, -122.1768858))
-                .title("Kirkland");
-
-        everett = new MarkerOptions()
-                .position(new LatLng(47.978748, -122.202001))
-                .title("Everett");
-
-        lynnwood = new MarkerOptions()
-                .position(new LatLng(47.819533, -122.32288))
-                .title("Lynnwood");
-
-        montlake = new MarkerOptions()
-                .position(new LatLng(47.7973733, -122.3281771))
-                .title("Montlake Terrace");
-
-        kent = new MarkerOptions()
-                .position(new LatLng(47.385938, -122.258212))
-                .title("Kent Valley");
-
-        showare = new MarkerOptions()
-                .position(new LatLng(47.38702, -122.23986))
-                .title("Showare Center");
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -70,13 +48,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         mGoogleMap = map;
-        mGoogleMap.addMarker(renton);
-        mGoogleMap.addMarker(kirkland);
-        mGoogleMap.addMarker(everett);
-        mGoogleMap.addMarker(lynnwood);
-        mGoogleMap.addMarker(montlake);
-        mGoogleMap.addMarker(kent);
-        mGoogleMap.addMarker(showare);
+        mGoogleMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(renton)
+                .add(kirkland)
+                .add(everret)
+                .add(lynnwood)
+                .add(montlake)
+                .add(kent)
+                .add(showare)
+                .add(renton));
         flyTo(SEATTLE);
     }
 
